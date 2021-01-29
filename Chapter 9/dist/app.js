2cab2a287e430618937bcc1d3fa09577521c3ad9
gfavaro-daitan <gfavaro@daitan.com>
2021-01-29 17:44:48 -0200

    Chapter 9

---
+++ Chapter 9/dist/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleElement;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
